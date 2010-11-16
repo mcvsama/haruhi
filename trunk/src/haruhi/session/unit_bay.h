111118c0acd6c35241f457c26060048a5e5b3fd5
Michał Gawron <michal@gawron.name>
2010-11-16 21:56:12 +0100

    Fixed #ifndefs.

--- trunk/src/haruhi/session/unit_bay.h
+++ trunk/src/haruhi/session/unit_bay.h
@@ -11,8 +11,8 @@
  * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
  */
 
-#ifndef HARUHI__UNIT_BAY_H__INCLUDED
-#define HARUHI__UNIT_BAY_H__INCLUDED
+#ifndef HARUHI__SESSION__UNIT_BAY_H__INCLUDED
+#define HARUHI__SESSION__UNIT_BAY_H__INCLUDED
 
 // Standard:
 #include <cstddef>
--- trunk/src/haruhi/settings/settings.h
+++ trunk/src/haruhi/settings/settings.h
@@ -11,8 +11,8 @@
  * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
  */
 
-#ifndef HARUHI__CONFIG_H__INCLUDED
-#define HARUHI__CONFIG_H__INCLUDED
+#ifndef HARUHI__SETTINGS__SETTINGS_H__INCLUDED
+#define HARUHI__SETTINGS__SETTINGS_H__INCLUDED
 
 // Standard:
 #include <cstddef>
