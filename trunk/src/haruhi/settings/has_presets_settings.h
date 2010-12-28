6dcc281ca6238bccf27eee8996e171d9af0da739
Michał Gawron <michal@gawron.name>
2010-12-28 13:10:53 +0100

    Fixing typos.

--- trunk/src/haruhi/settings/has_presets_settings.h
+++ trunk/src/haruhi/settings/has_presets_settings.h
@@ -11,8 +11,8 @@
  * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
  */
 
-#ifndef HARUHI__SETTINGS__PRESETABLE_SETTINGS_H__INCLUDED
-#define HARUHI__SETTINGS__PRESETABLE_SETTINGS_H__INCLUDED
+#ifndef HARUHI__SETTINGS__HAS_PRESETS_SETTINGS_H__INCLUDED
+#define HARUHI__SETTINGS__HAS_PRESETS_SETTINGS_H__INCLUDED
 
 // Standard:
 #include <cstddef>
