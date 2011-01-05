665495d0003efeb874e2188d125196e860c7a4a7
Michał Gawron <michal@gawron.name>
2011-01-06 00:28:39 +0100

    RefCounted utility class. Implements reference counting for objects allocated on heap.

---
+++ trunk/src/haruhi/utility/ref_counted.h
@@ -0,0 +1,69 @@
+/* vim:ts=4
+ *
+ * Copyleft 2008…2011  Michał Gawron
+ * Marduk Unix Labs, http://mulabs.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
+ */
+
+#ifndef HARUHI__UTILITY__MEMORY_H__INCLUDED
+#define HARUHI__UTILITY__MEMORY_H__INCLUDED
+
+// Standard:
+#include <cstddef>
+
+// Haruhi:
+#include <haruhi/utility/mutex.h>
+#include <haruhi/utility/noncopyable.h>
+
+
+/**
+ * Implements reference-counted object. Initial reference count is 1.
+ * When this number hits 0, it will be deleted. This means that objects
+ * of this type MUST be allocated on heap (using 'new' operator).
+ * Explicit deletion of this object is permitted.
+ */
+class RefCounted: public Noncopyable
+{
+  public:
+	RefCounted():
+		_ref_count (1)
+	{ }
+
+	/**
+	 * Increases references number.
+	 */
+	void
+	ref()
+	{
+		_ref_count += 1;
+	}
+
+	/**
+	 * Decreases references number. When number hits 0, object deletes itself
+	 * using 'delete' operator. So it is not safe to use object after unref() operation.
+	 */
+	void
+	unref()
+	{
+		if (--_ref_count == 0)
+			delete this;
+	}
+
+	/**
+	 * Returns number of references to this object.
+	 */
+	unsigned int
+	ref_num() const { return _ref_count; }
+
+  private:
+	unsigned int _ref_count;
+};
+
+#endif
+
