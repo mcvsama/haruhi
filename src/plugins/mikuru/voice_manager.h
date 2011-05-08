9260503cb8564e1ce3f3aa9aad95a28caf0eecae
Michał Gawron <michal@gawron.name>
2011-05-08 14:13:13 +0200

    Updates header files.

--- src/plugins/mikuru/voice_manager.h
+++ src/plugins/mikuru/voice_manager.h
@@ -11,8 +11,8 @@
  * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
  */
 
-#ifndef HARUHI__UNITS__MIKURU__VOICE_MANAGER_H__INCLUDED
-#define HARUHI__UNITS__MIKURU__VOICE_MANAGER_H__INCLUDED
+#ifndef HARUHI__PLUGINS__MIKURU__VOICE_MANAGER_H__INCLUDED
+#define HARUHI__PLUGINS__MIKURU__VOICE_MANAGER_H__INCLUDED
 
 // Standard:
 #include <cstddef>
