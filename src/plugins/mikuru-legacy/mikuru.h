d1bcd498e4a2b59c3368401a96171afeba900a47
Michał Gawron <michal@gawron.name>
2011-08-23 16:48:55 +0200

    Update #ifndef guard.

--- src/plugins/mikuru-legacy/mikuru.h
+++ src/plugins/mikuru-legacy/mikuru.h
@@ -11,8 +11,8 @@
  * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
  */
 
-#ifndef HARUHI__PLUGINS__MIKURU__MIKURU_H__INCLUDED
-#define HARUHI__PLUGINS__MIKURU__MIKURU_H__INCLUDED
+#ifndef HARUHI__PLUGINS__MIKURU_LEGACY__MIKURU_H__INCLUDED
+#define HARUHI__PLUGINS__MIKURU_LEGACY__MIKURU_H__INCLUDED
 
 // Standard:
 #include <cstddef>
